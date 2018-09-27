6854171724efc866e2d376b206249167c1580dce
Ben Johnson <thisguyistotallyben@gmail.com>
2018-09-26 22:37:08 -0400

    added typing feedback for callsign/conditions lookup

--- commands/lookup.py
+++ commands/lookup.py
@@ -13,24 +13,26 @@ param callsign: The callsign, which must be a string
 '''
 @bot.command()
 async def call(ctx, callsign:str):
-    result = call_lookup.lookup(callsign)
+    with ctx.typing():
+        result = call_lookup.lookup(callsign)
 
-    # send it
-    await ctx.send(embed=result)
+        # send it
+        await ctx.send(embed=result)
 
 '''
 Band conditions sourced from hamqsl
 '''
 @bot.command()
 async def cond(ctx):
-    # remove possibly conficting old file
-    if os.path.isfile("conditions.gif"):
-        os.remove("conditions.gif")
+    with ctx.typing():
+        # remove possibly conficting old file
+        if os.path.isfile("conditions.gif"):
+            os.remove("conditions.gif")
 
-    # download the latest conditions
-    r = requests.get('https://www.hamqsl.com/solar101pic.php')
-    open('conditions.gif', 'wb').write(r.content)
+        # download the latest conditions
+        r = requests.get('https://www.hamqsl.com/solar101pic.php')
+        open('conditions.gif', 'wb').write(r.content)
 
-    # send it
-    f = discord.File('conditions.gif')
-    await ctx.send(file=f)
+        # send it
+        f = discord.File('conditions.gif')
+        await ctx.send(file=f)
