f4314ac84a37ee6a83e7b304628dbedf58173aef
Ben <thisguyistotallyben@gmail.com>
2020-04-18 02:40:26 -0400

    Lookup and Code Cleanup - Lookup has begun   - Conditions are working   - Callsign lookup is going to require a little refactoring - Code cleanup   - Just removing old commented out code as I get the new stuff done

--- modules/lookup.py
+++ modules/lookup.py
@@ -1,40 +1,16 @@
 import os
 import requests
-import xml.etree.ElementTree as et
 import discord
-
-from hamtheman import bot, call_lookup
-from onlinelookup import olerror
-
-
-'''
-Callsign lookup
-
-param callsign: The callsign, which must be a string
-'''
+from discord.ext import commands
+from onlinelookup import hamqth, callook, olerror
 
 
-@bot.command()
-async def call(ctx, callsign: str, *, params: str=None):
-    if params is not None:
-        params = params.split()
-    print(callsign, params)
+class LookupCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    async with ctx.typing():
-        result = call_lookup.lookup(callsign, params)
-
-        # send it
-        await ctx.send(embed=result)
-
-
-'''
-Band conditions sourced from hamqsl
-'''
-
-
-@bot.command()
-async def cond(ctx):
-    async with ctx.typing():
+    @commands.command()
+    async def cond(self, ctx):
         # remove possibly conficting old file
         if os.path.isfile("conditions.gif"):
             os.remove("conditions.gif")
@@ -43,16 +19,88 @@ async def cond(ctx):
         r = requests.get('https://www.hamqsl.com/solar101pic.php')
         open('conditions.gif', 'wb').write(r.content)
 
-        # send it
-        f = discord.File('conditions.gif')
-    await ctx.send(file=f)
+        with open('conditions.gif', 'rb') as f:
+            await ctx.send(file=discord.File(f, 'conditions.gif'))
+
+    @commands.command()
+    async def call(self, ctx, callsign: str):
+        await ctx.send(callsign)
 
 
-@bot.command()
-async def newcond(ctx):
-    r = requests.get('http://www.hamqsl.com/solarxml.php')
+def setup(bot):
+    bot.add_cog(LookupCog(bot))
 
-    data = et.parse(r.content)
-    root = data.getroot()
-    print(root)
-    await ctx.send(r.content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import requests
+# import xml.etree.ElementTree as et
+# import discord
+
+# from hamtheman import bot, call_lookup
+# from onlinelookup import olerror
+
+
+# '''
+# Callsign lookup
+
+# param callsign: The callsign, which must be a string
+# '''
+
+
+# @bot.command()
+# async def call(ctx, callsign: str, *, params: str=None):
+#     if params is not None:
+#         params = params.split()
+#     print(callsign, params)
+
+#     async with ctx.typing():
+#         result = call_lookup.lookup(callsign, params)
+
+#         # send it
+#         await ctx.send(embed=result)
+
+
+# '''
+# Band conditions sourced from hamqsl
+# '''
+
+
+# @bot.command()
+# async def cond(ctx):
+#     async with ctx.typing():
+#         # remove possibly conficting old file
+#         if os.path.isfile("conditions.gif"):
+#             os.remove("conditions.gif")
+
+#         # download the latest conditions
+#         r = requests.get('https://www.hamqsl.com/solar101pic.php')
+#         open('conditions.gif', 'wb').write(r.content)
+
+#         # send it
+#         f = discord.File('conditions.gif')
+#     await ctx.send(file=f)
+
+
+# @bot.command()
+# async def newcond(ctx):
+#     r = requests.get('http://www.hamqsl.com/solarxml.php')
+
+#     data = et.parse(r.content)
+#     root = data.getroot()
+#     print(root)
+#     await ctx.send(r.content)
--- modules/misc.py
+++ modules/misc.py
@@ -4,6 +4,13 @@ from discord.ext import commands
 from datetime import datetime, timedelta
 
 
+'''
+TODO:
+- Oof counter
+  - Make this server specific
+'''
+
+
 class MiscCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -41,6 +48,15 @@ class MiscCog(commands.Cog):
             .generate(
                 title="Help",
                 description=help_message
+            )
+        )
+
+    @commands.command()
+    async def about(self, ctx):
+        await ctx.send(embed=self.embed_service
+            .generate(
+                title="Help",
+                description=htm_about + self.calc_uptime()
             )
         )
 
@@ -83,107 +99,6 @@ def setup(bot):
     bot.add_cog(MiscCog(bot))
 
 
-
-
-
-
-
-
-
-
-
-
-
-# '''
-# UTC time
-# '''
-
-
-# @bot.command()
-# async def utc(ctx):
-#     s = str(datetime.utcnow())
-#     ss = s.strip().split()
-#     date = ss[0]
-#     time = ss[1][0:8]
-
-#     # send it
-#     em = discord.Embed(title='Universal Coordinated Time',
-#                        description=f'**Date:** {date}\n**Time:** {time}',
-#                        color=0x00c0ff)
-#     await ctx.send(embed=em)
-
-
-# '''
-# Kerchunk like a repeater
-# '''
-
-
-# @bot.command()
-# async def kerchunk(ctx):
-#     await ctx.send(htm_kerchunk)
-
-
-# '''
-# Standards
-# '''
-
-
-# @bot.command()
-# async def standards(ctx):
-#     await ctx.send('https://xkcd.com/927')
-
-
-# '''
-# Help dialog
-# '''
-
-
-# @bot.command()
-# async def help(ctx):
-#     await ctx.send(embed=help_embed)
-
-
-# '''
-# About dialog
-# '''
-
-
-# @bot.command()
-# async def about(ctx):
-#     abstr = htm_about + calc_uptime()
-#     em = discord.Embed(title='About HamTheMan',
-#                        description=abstr,
-#                        color=0x00c0ff)
-#     em = em.set_footer(text='Version 2.1')
-
-#     # send it
-#     await ctx.send(embed=em)
-
-
-# '''
-# Uptime
-# '''
-
-
-# @bot.command()
-# async def uptime(ctx):
-#     await ctx.send(calc_uptime())
-
-
-
-
-
-# '''
-# Oof counter
-# TODO: make this server specific
-# '''
-
-
-# @bot.command()
-# async def oofs(ctx):
-#     await ctx.send(f"Number of oofs since last reboot: {config['oofs']}")
-
-
 '''
 STRINGS AND STUFF
 '''
@@ -207,9 +122,6 @@ help_message = ('**Core commands**\n'
                 'reboot\n'
                 '\n**This bot is also responsible for the oofs and bonks**')
 
-help_embed = discord.Embed(title='Help: Preface commands with \'htm\'',
-                           description=help_message,
-                           color=0x00c0ff)
 
 htm_about = ('**Author**\n'
              '\tBen Johnson, AB3NJ\n'
@@ -226,5 +138,3 @@ htm_about = ('**Author**\n'
              '\thttps://discordapp.com/api/oauth2/authorize?client_id='
              '368613435543650314&permissions=8&scope=bot\n'
              '\n**Uptime**\n\t')
-
-htm_kerchunk = 'H...A...M...T...H...E...M...A...N...Repeater *kksshh*'
